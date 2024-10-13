ed8bb5d7067ef8aa0cfe25b8ae64c2d59437a478
Mohammed Afsar <afsar.muazam03@gmail.com>
2024-10-13 19:22:06 +0530

    User-1

---
+++ src/UserComponent/LoginUser.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "../UserComponent/LoginUser.css"
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBCheckbox,
+    MDBIcon
+  }
+  from 'mdb-react-ui-kit';
+
+function LoginUser(){
+    return (
+        <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
+
+      <MDBRow>
+
+        <MDBCol md='6' className='position-relative'>
+
+          <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+          <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+
+          <MDBCard className='my-5 bg-glass'>
+            <MDBCardBody className='p-5'>
+
+              <MDBRow>
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                </MDBCol>
+
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email'/>
+              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password'/>
+
+              <div className='d-flex justify-content-center mb-4'>
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+              </div>
+
+              <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
+
+              <div className="text-center">
+
+                <p>or sign up with:</p>
+
+              </div>
+
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+
+      </MDBRow>
+
+    </MDBContainer>
+    );
+
+}
+
+export default LoginUser;
